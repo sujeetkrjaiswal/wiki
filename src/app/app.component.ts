b23d308aeeea615ededc0248165ec16403b6d06c
Sujeet Jaiswal <sujeet@Aviseks-MacBook-Pro.local>
2019-09-29 01:18:16 +0530

    angular initial setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wiki-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'wiki';
+}
